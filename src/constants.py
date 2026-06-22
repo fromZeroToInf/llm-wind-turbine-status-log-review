058af3ffcee0bfc28bcdc836e2e30df125ea0ded
Shin-Young Hur <fromZeroToInf@outlook.de>
2026-06-22 22:24:37 +0200

    deep search almost completed

--- src/constants.py
+++ src/constants.py
@@ -8,8 +8,11 @@ from string import Template
 PATH_PROJECT_ROOT =define_project_root_path()
 PATH_STATUS_LOGS = PATH_PROJECT_ROOT/"examples/status_logs/"
 PATH_DETECTIONS = PATH_PROJECT_ROOT/"examples/detections/"
-PATH_OUTPUT = PATH_PROJECT_ROOT/"src"/"results"/"output"/"out.jsonl"
-PATH_LAST_IDX = PATH_PROJECT_ROOT/"src"/"results"/"index"/"idx.jsonl"
+PATH_OUTPUT = PATH_PROJECT_ROOT/"src"/"results"/"output"
+PATH_LAST_IDX = PATH_PROJECT_ROOT/"src"/"results"/"index"
+
+FN_OUTPUT = "out.jsonl"
+FN_IDX = "idx.jsonl"
 """
 Set the columns of the status logs    
 """
@@ -51,7 +54,7 @@ COLS_TO_ROUND = ["re_at_ts",
 """
 Control tokens
 """
-MAX_TOKENS= 2048
+MAX_TOKENS= 10000
 
 SYSTEM_PROMPT: Template = Template("""
         You are a data analyst specialized in wind turbine SCADA data anomaly detection and also a specialist in wind turbine operating and a wind turbine engineer.
@@ -95,6 +98,11 @@ SYSTEM_PROMPT: Template = Template("""
         - no_relevant_anomaly_evidence: choose this if neither the status logs nor the anomaly statistics provide meaningful evidence.
         - inconclusive: choose this if the information is insufficient, contradictory, or too weak for a clear assessment.
 
+        Output constraints:
+        - Keep anomaly_description_reasoning to one short sentence.
+        - Keep each log reasoning to one short sentence.
+        - Do not write long explanations.
+        
         Return valid JSON only.
         Do not include markdown, comments, or explanations outside the JSON.
     """)
@@ -130,27 +138,27 @@ USER_PROMPT: Template = Template(
 
         Return exactly this JSON structure:
 
-        {{
+        {
         "wt_id": "$wt_id",
         "detection_id": "$detection_id",
         "detection_ts": "$detection_ts",
         "relevant_signal": "$signal_name",
         "anomaly_description_reasoning": "...",
         "relevant_logs": [
-            {{
+            {
             "log_index": "...",
             "log_start_ts": "...",
             "log_end_ts": "...",
             "reasoning": "..."
-            }}
+            }
         ],
         "overall_assessment": "documented_relevant_event | possibly_undocumented_anomaly | no_relevant_anomaly_evidence | inconclusive",
         "overall_reasoning": "..."
-        }}
+        }
 
         If no relevant status log exists, return:
 
-        {{
+        {
         "wt_id": "$wt_id",
         "detection_id": "$detection_id",
         "detection_ts": "$detection_ts",
@@ -159,6 +167,6 @@ USER_PROMPT: Template = Template(
         "relevant_logs": [],
         "overall_assessment": "possibly_undocumented_anomaly | no_relevant_anomaly_evidence | inconclusive",
         "overall_reasoning": "..."
-        }}
+        }
         """
 )
--- src/reviewer.py
+++ src/reviewer.py
@@ -1,9 +1,16 @@
 import pandas as pd
+import jsonlines
+import os
 from pathlib import Path
 from typing import Any
-from src import helper, llm, parser, prompt, constants
+from src import helper, llm, parser, prompt
 from src.llm import LLMProvider
 from src.schema import LLMOutput
+from src import constants as cts
+import warnings
+import time
+import random as rnd
+
 
 def review_detection(
     detection: pd.Series,
@@ -30,10 +37,10 @@ def review_detection(
                         ts_cols=ts_cols,
                         delta_time=delta_time,
                         chunk_size=chunk_size,
-                        wt_col=constants.WT_ID)
+                        wt_col=cts.WT_ID)
     
     
-    chunks = [prompt.format_chunk_logs(chunk_log=chunk, columns=constants.LOG_COLS) for chunk in chunks]
+    chunks = [prompt.format_chunk_logs(chunk_log=chunk, columns=cts.LOG_COLS) for chunk in chunks]
     system_prompt = prompt.build_system_prompt()
     
     prompts = [prompt.build_user_prompt(detection_id=det_idx, detection_ts=det_ts, wt_id=wt_id, signal_name=signal_name, logs_text=chunk,det_stats=detection) for chunk in chunks]
@@ -55,41 +62,104 @@ def review_detection(
             answers.relevant_logs.extend(answer.relevant_logs)
     
     return answers
+
+def _write_jsonl(file_path:Path, obj:dict, mode:str)-> None:
+    with jsonlines.open(file_path, mode=mode) as writer:
+            writer.write(obj)
+
+def _get_idx(file_path:Path)-> int:
     
+    last_idx: int = -1
+    if file_path.exists():
+        with jsonlines.open(file_path) as reader:
+            row = next(iter(reader), None)
+            if row is not None:
+                last_idx = int(row["idx"])
+    else:
+        _write_jsonl(file_path, {"idx": 0}, mode="w")
+        last_idx = 0
+    return last_idx
+            
+def _all_results(file_path: Path) -> list[dict]:
+    results:list[dict] = []
+    if file_path.exists():
+        with jsonlines.open(file_path, mode="r") as reader:
+            for obj in reader:
+                results.append(LLMOutput.model_validate(obj))
+    return results
+
 def deep_search(
     detections:pd.DataFrame,
     log_path: list[Path],
     provider: LLMProvider,
-    ts_cols: list[str]= constants.TS_COLS,
+    ts_cols: list[str]= cts.TS_COLS,
     delta_time: pd.Timedelta= pd.Timedelta(days=30),
-    detection_ts_col: str = constants.DET_TS_COL,
-    wt_id_col: str = constants.WT_ID,
-    signal_col: str = constants.SIGNAL,
+    detection_ts_col: str = cts.DET_TS_COL,
+    wt_id_col: str = cts.WT_ID,
+    signal_col: str = cts.SIGNAL,
     model: str = "llama3.2:3b",
     chunk_size: int = 50,
+    max_retries: int = 3,
+    wait_seconds: float = 30.0,
 ) -> list[LLMOutput]:
     
-    output_list: list[LLMOutput]= []
+    wait_seconds = wait_seconds
     
-    for _,row in detections.iterrows():
-        wt_id = row[wt_id_col]
-        log_fp = [p for p in log_path if f"_ID_{wt_id}." in p.name][0]
-        
-        if not log_fp:
-            raise FileNotFoundError(f"Status-log file not found with wt id:{wt_id}")
-        
-        output = review_detection(
-            detection=row,
-            log_file_path=log_fp,
-            delta_time=delta_time,
-            ts_cols=ts_cols,
-            detection_ts_col=detection_ts_col,
-            wt_id_col=wt_id_col,
-            signal_col=signal_col,
-            provider=provider,
-            model=model,
-            chunk_size=chunk_size)
-        
-        output_list.append(output)
+    detections= detections.reset_index()
     
-    return output_list
+    cts.PATH_OUTPUT.mkdir(parents=True, exist_ok=True)
+    output_file_path  = cts.PATH_OUTPUT / cts.FN_OUTPUT
+    
+    cts.PATH_LAST_IDX.mkdir(parents=True, exist_ok=True)
+    idx_path = cts.PATH_LAST_IDX / cts.FN_IDX
+    
+    last_idx = _get_idx(idx_path)
+    
+    for idx,row in detections.iterrows():
+        
+        if idx < last_idx:
+            continue
+        attempts= 0
+        
+        while attempts <= max_retries:
+            try:
+                wt_id = row[wt_id_col]
+                
+                matching_file = [p for p in log_path if f"_ID_{wt_id}." in p.name][0]
+                
+                if not matching_file:
+                    raise FileNotFoundError(f"Status-log file not found with wt id:{wt_id}")
+                
+                output = review_detection(
+                    detection=row,
+                    log_file_path=matching_file,
+                    delta_time=delta_time,
+                    ts_cols=ts_cols,
+                    detection_ts_col=detection_ts_col,
+                    wt_id_col=wt_id_col,
+                    signal_col=signal_col,
+                    provider=provider,
+                    model=model,
+                    chunk_size=chunk_size)
+                
+                _write_jsonl(output_file_path,obj=output.model_dump(), mode="a")
+                _write_jsonl(idx_path, obj={"idx": idx +1}, mode="w")
+                last_idx = idx
+                time.sleep(10)
+                break
+            except Exception as exc:
+                attempts +=1
+                warnings.warn(f"Error at detection idx={idx}\n"
+                              f"attempt={attempts}, max retries={max_retries}\n"
+                              f"Exc. Msg: {exc}")
+                
+                if attempts > max_retries:
+                    warnings.warn(f"Deep search stopped at idx={idx}\n"
+                                  f"Restart later to continue from this detection.\n")
+                    return _all_results(output_file_path)
+                
+                time.sleep(wait_seconds)
+                wait_seconds += wait_seconds*rnd.uniform(0.1, 2.5)
+    
+    #_write_jsonl(idx_path, obj={"idx": 0}, mode="w")
+    return _all_results(output_file_path)
